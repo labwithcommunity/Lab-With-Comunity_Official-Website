c2f046ba2506f1aadeb92f3338d938d9ca4b1ec5
Seba9989 <sgrzybbowski@gmail.com>
2024-09-20 13:51:17 +0200

    Default Input

---
+++ src/stories/Atoms/Input/Input.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+
+	export let value = '';
+
+	export let type: 'email' | 'text' | 'password' | 'number' = 'text';
+	export let name: string;
+	export let placeholder = '';
+
+	export let state: Optional<'success' | 'error'> = null;
+
+	export let required = true;
+	export let disabled = false;
+
+	let show: boolean = true;
+</script>
+
+<label
+	class="flex w-full rounded-md border border-gray-600 p-1 text-gray-400"
+	class:success={state == 'success'}
+	class:error={state == 'error'}
+>
+	<input
+		class="w-full bg-transparent focus:outline-none"
+		{placeholder}
+		id={name}
+		{name}
+		{...{ type: show ? type : 'text' }}
+		{required}
+		{disabled}
+		bind:value
+	/>
+	{#if type == 'password'}
+		<button on:click={() => (show = !show)}>
+			{#if show == false}
+				<Icon icon="tabler:eye" class="my-auto" />
+			{:else}
+				<Icon icon="tabler:eye-closed" />
+			{/if}
+		</button>
+	{/if}
+</label>
