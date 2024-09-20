185d066e777a91e850c61da9c352365dd996d5d9
Seba9989 <sgrzybbowski@gmail.com>
2024-09-20 13:47:28 +0200

    final storybook fix

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -8,6 +8,15 @@ const preview: Preview = {
 				color: /(background|color)$/i,
 				date: /Date$/i
 			}
+		},
+		backgrounds: {
+			values: [
+				// 👇 Default values
+				{ name: 'Dark', value: '#0f172a' },
+				{ name: 'Light', value: '#f1f5f9' }
+			],
+			// 👇 Specify which background is shown by default
+			default: 'Dark'
 		}
 	}
 };
