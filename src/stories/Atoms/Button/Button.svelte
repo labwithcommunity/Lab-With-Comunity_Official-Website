19ada07ca5e0ad1841102e0d96e8fcd89f77589a
Seba9989 <sgrzybbowski@gmail.com>
2024-09-20 13:51:58 +0200

    Default Button

---
+++ src/stories/Atoms/Button/Button.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	export let type: 'button' | 'submit' | 'reset' = 'button';
+</script>
+
+<button on:click {type} class="text rounded-md bg-gray-400 p-2 text-gray-800">
+	<slot />
+</button>
