3455360b40a7717f175fe55d27e4e62cfc7789b5
Seba9989 <sgrzybbowski@gmail.com>
2024-10-01 19:34:32 +0200

    Button styles refactor

--- src/stories/Atoms/Button/Button.svelte
+++ src/stories/Atoms/Button/Button.svelte
@@ -1,7 +1,24 @@
 <script lang="ts">
 	export let type: 'button' | 'submit' | 'reset' = 'button';
+	export let isError = false;
 </script>
 
-<button on:click {type} class="{$$props.class} text rounded-md bg-gray-400 p-2 text-gray-800">
+<button
+	on:click
+	{type}
+	class:error={isError}
+	class="{$$props.class} text rounded-md bg-primary-200 p-2 font-black text-secondary-300 drop-shadow-sm"
+>
 	<slot />
 </button>
+
+<style lang="postcss">
+	button {
+		font-family: 'Playfair Display', serif;
+	}
+
+	.error {
+		background-color: theme(colors.error.500);
+		color: theme(colors.primary.100);
+	}
+</style>
